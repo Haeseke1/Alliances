4d1199ada822f8cbbb898378c5ba2270f84e83fd
BuildTools <unconfigured@null.spigotmc.org>
2016-12-31 14:47:59 +0100

    added pluginmanager

--- src/me/Haeseke1/Alliances/Main/Main.java
+++ src/me/Haeseke1/Alliances/Main/Main.java
@@ -1,5 +1,7 @@
 package me.Haeseke1.Alliances.Main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -8,7 +10,7 @@ public class Main extends JavaPlugin{
 	
 	@Override
 	public void onEnable() {
-		
+		PluginManager pm = Bukkit.getPluginManager();
 	}
 	
 	
