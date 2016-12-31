167e341cba4410eb5ca3110fed8098f0cfd107d4
BuildTools <unconfigured@null.spigotmc.org>
2016-12-31 14:43:34 +0100

    added Enable Disable

--- src/me/Haeseke1/Alliances/Main/Main.java
+++ src/me/Haeseke1/Alliances/Main/Main.java
@@ -1,5 +1,21 @@
 package me.Haeseke1.Alliances.Main;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	
 
 }
