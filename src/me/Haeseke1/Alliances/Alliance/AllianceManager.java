a20cbe1eba1c175e37ad5ae9f9ff5a0b7c8f997d
BuildTools <unconfigured@null.spigotmc.org>
2017-01-03 15:10:25 +0100

    clean up

--- src/me/Haeseke1/Alliances/Alliance/AllianceManager.java
+++ src/me/Haeseke1/Alliances/Alliance/AllianceManager.java
@@ -8,26 +8,27 @@ import me.Haeseke1.Alliances.Main.Main;
 import me.Haeseke1.Alliances.Utils.MessageManager;
 
 public class AllianceManager {
- public static void createAlliance(Player owner){
-	if(!playerIsInAlli(owner)){
-	 int mCoins = Coins.getPlayerCoins(owner);
-	 int mWins = 0;
-	 int mLoses = 0;
-	 Alliance alliance = new Alliance(owner,mWins,mLoses,mCoins);
-	 Main.alliances.add(alliance);
-	 alliance.getMembers().put(owner.getUniqueId(), "");
-	 return;
-	   }
-	MessageManager.sendAlertMessage(owner, "You're already in an alliance.");
-	return;
- }
- 
- public static boolean playerIsInAlli(Player player){
-	 for(Alliance alli: Main.alliances){
-		 if(alli.getMembers().containsKey(player.getUniqueId())){
-			 return true;
-		 }
-	 }
-	 return false;
- }
+
+	public static void createAlliance(Player owner) {
+		if (!playerIsInAlli(owner)) {
+			int mCoins = Coins.getPlayerCoins(owner);
+			int mWins = 0;
+			int mLoses = 0;
+			Alliance alliance = new Alliance(owner, mWins, mLoses, mCoins);
+			Main.alliances.add(alliance);
+			alliance.getMembers().put(owner.getUniqueId(), "");
+			return;
+		}
+		MessageManager.sendAlertMessage(owner, "You're already in an alliance.");
+		return;
+	}
+
+	public static boolean playerIsInAlli(Player player) {
+		for (Alliance alli : Main.alliances) {
+			if (alli.getMembers().containsKey(player.getUniqueId())) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
