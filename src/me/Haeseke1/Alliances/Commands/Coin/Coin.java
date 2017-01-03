f15402adcbd10d465f99022f9aa9af96fd587109
BuildTools <unconfigured@null.spigotmc.org>
2017-01-03 13:36:09 +0100

    added command "pay"

--- src/me/Haeseke1/Alliances/Commands/Coin/Coin.java
+++ src/me/Haeseke1/Alliances/Commands/Coin/Coin.java
@@ -13,6 +13,7 @@ public class Coin {
 			sender.sendMessage(MessageManager.infoColorCode + "===== Coins =====");
 			sender.sendMessage(MessageManager.infoColorCode + "Commands:");
 			sender.sendMessage(MessageManager.infoColorCode + "/... coin balance #Watch your balance or from other Players");
+			sender.sendMessage(MessageManager.infoColorCode + "/... coin pay <Player> <Amount> #Watch your balance or from other Players");
 			sender.sendMessage(MessageManager.infoColorCode + "/... coin add <Player> <Amount> #give player coins");
 			return ;
 		}
@@ -45,7 +46,31 @@ public class Coin {
 			}
 			return;
 		}
-		
+		if(args[1].equalsIgnoreCase("pay")){
+			if(args.length < 4){
+				MessageManager.sendAlertMessage((Player) sender, "Not enough arguments! Use /alliance coin for more information!");
+				return;
+			}else{
+				if(!(sender instanceof Player)){
+					MessageManager.sendAlertMessage((Player) sender, "You need to be a player to do this command!");
+					return;
+				}
+				Player player = (Player) sender;
+				try{
+					if(Coins.removePlayerCoins(player, Integer.parseInt(args[3]))){
+						Coins.addPlayerCoins(args[2], Integer.parseInt(args[3]));
+						MessageManager.sendRemarkMessage((Player) sender, "Succesfully payed coins! Your new balance: " + Coins.getPlayerCoins(player));
+						return;
+					}else{
+						MessageManager.sendAlertMessage((Player) sender, "You don't have enough coins!");
+						return;
+					}
+				}catch(Exception e){
+					MessageManager.sendAlertMessage((Player) sender, "Given arguments are not correct!");
+					return;
+				}
+			}
+		}
 		MessageManager.sendAlertMessage((Player) sender, "Wrong arguments! use /alliance coin for more information!");
 		
 	}
