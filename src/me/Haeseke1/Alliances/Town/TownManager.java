a6c2af1de55eebcfdd3e728ae501991e3843ffd5
Pieter Herlitska <pieter.herlitska@gmail.com>
2017-01-20 22:09:50 +0100

    Fixed towns

--- src/me/Haeseke1/Alliances/Town/TownManager.java
+++ src/me/Haeseke1/Alliances/Town/TownManager.java
@@ -1,6 +1,8 @@
 package me.Haeseke1.Alliances.Town;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
@@ -76,18 +78,16 @@ public class TownManager {
 		return null;
 	}
 
-    public static void registerTowns(){
+    public static void saveTowns(){
     	for(Alliance al: Main.alliances){
-    		int count = 0;
     		for(Town town: al.getTowns()){
     			int chunkCount = 0;
     			for(Chunk chunk: town.chunks){
-    				allianceConfig.set(al.getName() + ".towns." + count + ".chunks." + chunkCount + ".x", chunk.getX());
-    				allianceConfig.set(al.getName() + ".towns." + count + ".chunks." + chunkCount + ".z", chunk.getZ());
-    				allianceConfig.set(al.getName() + ".towns." + count + ".chunks." + chunkCount + ".world", chunk.getWorld().getName());
+    				allianceConfig.set(al.getName() + ".towns." + town.name + ".chunks." + chunkCount + ".x", chunk.getX());
+    				allianceConfig.set(al.getName() + ".towns." + town.name + ".chunks." + chunkCount + ".z", chunk.getZ());
+    				allianceConfig.set(al.getName() + ".towns." + town.name + ".chunks." + chunkCount + ".world", chunk.getWorld().getName());
     				chunkCount ++;
     			}
-    			count ++;
     		}
     	}
     }
@@ -95,7 +95,19 @@ public class TownManager {
     public static void loadTowns(){
     	for(String alliancename: allianceConfig.getConfigurationSection("").getKeys(false)){
     		Alliance al = AllianceManager.getAlliance(alliancename);
-    	    
+    	    for(String townName: allianceConfig.getConfigurationSection(alliancename + ".towns").getKeys(false)){
+    	    	Town town = new Town(townName,null,al);
+    	    	for(String chunk: allianceConfig.getConfigurationSection(alliancename + ".towns." + townName + ".chunks").getKeys(false)){
+    	    		int chunkNumber = Integer.parseInt(chunk);
+    	    		int x = allianceConfig.getInt(alliancename + ".towns." + townName + ".chunks." + chunkNumber + ".x");
+    	    		int z = allianceConfig.getInt(alliancename + ".towns." + townName + ".chunks." + chunkNumber + ".z");
+    	    		String world = allianceConfig.getString(alliancename + ".towns." + townName + ".chunks." + chunkNumber + ".world");
+    	    		Location loc = new Location(Bukkit.getWorld(world),x,0,z);
+    	    		Chunk tchunk = loc.getChunk();
+    	    		town.addChunck(tchunk);
+    	    	}
+    	    	Town.towns.add(town);
+    	    }
     	}
     }
 }
