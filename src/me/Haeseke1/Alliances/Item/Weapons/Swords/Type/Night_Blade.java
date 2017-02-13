51cf2846d4b02ccf79479219ac79984587bcfe9e
BuildTools <unconfigured@null.spigotmc.org>
2017-02-13 10:31:02 +0100

    update

--- src/me/Haeseke1/Alliances/Item/Weapons/Swords/Type/Night_Blade.java
+++ src/me/Haeseke1/Alliances/Item/Weapons/Swords/Type/Night_Blade.java
@@ -1,16 +1,76 @@
 package me.Haeseke1.Alliances.Item.Weapons.Swords.Type;
 
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import me.Haeseke1.Alliances.Alliance.Alliance;
+import me.Haeseke1.Alliances.Alliance.AllianceManager;
 import me.Haeseke1.Alliances.Item.Weapons.Swords.Sword;
+import me.Haeseke1.Alliances.Item.Weapons.Swords.SwordManager;
+import me.Haeseke1.Alliances.Utils.MessageManager;
+import me.Haeseke1.Alliances.Utils.SoundManager;
 
-public class Night_Blade extends Sword{
+public class Night_Blade extends Sword implements Listener{
 
 	
-	public Night_Blade(String name, Player player, int cooldown, String effect) {
-		super(name, player, cooldown, effect);
+	public Night_Blade(Player player, int cooldown) {
+		super("Night Blade", player, cooldown);
 	}
 	
 	
+	
+	public Night_Blade() {
+		
+	}
+
+
+
+	@EventHandler
+	public void onRightClick(PlayerInteractEvent event){
+	    Player player = event.getPlayer();
+	    if(player.getItemInHand() == null){
+	    	return;
+	    }
+		if(event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK){
+			return;
+		}
+		if(!player.getItemInHand().hasItemMeta() || !player.getItemInHand().getItemMeta().hasDisplayName() && !player.getItemInHand().getItemMeta().hasLore()){
+			return;
+		}
+		String displayname = player.getItemInHand().getItemMeta().getDisplayName();
+		if(!displayname.equals(ChatColor.LIGHT_PURPLE + "Night Blade")){
+			return;
+		}
+		if(!SwordManager.hasSword(player.getUniqueId(), "Night Blade")){
+			Night_Blade night_blade = new Night_Blade(player, 120);
+			night_blade.giveEffect(Sound.ENDERDRAGON_WINGS);
+		}else{
+			MessageManager.sendMessage(player, "&cThis ability is cooling down (&6" + SwordManager.getSword(player.getUniqueId(), name).cooldown + "&cs left)"); 
+		}
+	}
+	
+	
+	public void giveEffect(Sound sound){
+		playerRemoveEffect(PotionEffectType.INVISIBILITY);
+		player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY,160,1,false));
+		if(sound != null){ 
+			SoundManager.playSoundToPlayer(sound, player);
+		}
+		for(Player player: Bukkit.getOnlinePlayers()){
+			Alliance al = AllianceManager.getAlliance(this.player);
+			if(al.equals(AllianceManager.getAlliance(player))){
+				continue;
+			}
+			player.hidePlayer(this.player);
+		}
+	}
 }
--- src/me/Haeseke1/Alliances/Item/Weapons/Swords/Type/Warrior_Sword.java
+++ src/me/Haeseke1/Alliances/Item/Weapons/Swords/Type/Warrior_Sword.java
@@ -1,14 +1,60 @@
 package me.Haeseke1.Alliances.Item.Weapons.Swords.Type;
 
+import java.util.Random;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import me.Haeseke1.Alliances.Item.Weapons.Swords.Sword;
+import me.Haeseke1.Alliances.Item.Weapons.Swords.SwordManager;
+import me.Haeseke1.Alliances.Utils.SoundManager;
 
-public class Warrior_Sword extends Sword{
+public class Warrior_Sword extends Sword implements Listener{
 
-	public Warrior_Sword(String name, Player player, int cooldown, String effect) {
-		super(name, player, cooldown, effect);
+	public Warrior_Sword(Player player, int cooldown) {
+		super("Warrior Sword", player, cooldown);
 		
 	}
-
+	
+	public Warrior_Sword() {
+		
+	}
+	
+	@EventHandler
+	public static void onRightClick(EntityDamageByEntityEvent event){
+		if(!(event.getDamager() instanceof Player)){
+			return;
+		}
+		Player player = (Player) event.getDamager();
+		if(player.getItemInHand() == null || player.getItemInHand().getType() == Material.AIR){
+			return;
+		}
+		if(!player.getItemInHand().hasItemMeta() || !player.getItemInHand().getItemMeta().hasDisplayName() && !player.getItemInHand().getItemMeta().hasLore()){
+			return;
+		}
+		String displayname = player.getItemInHand().getItemMeta().getDisplayName();
+		if(!displayname.equals(ChatColor.RED + "Warrior Sword")){
+			return;
+		}
+		int number = new Random().nextInt(10);
+		if(!SwordManager.hasSword(player.getUniqueId(), "Warrior Sword") && 0 == number){
+		    Warrior_Sword warrior_sword = new Warrior_Sword(player,1);
+		    warrior_sword.healPlayer(Sound.LEVEL_UP);
+		}
+	}
+	
+	
+	public void healPlayer(Sound sound){
+		if(player.getHealth() <= player.getMaxHealth() - 1){
+			player.setHealth(player.getHealth() + 1);
+		}
+		SoundManager.playSoundToPlayer(sound, player);
+	}
+	
+	
 }
