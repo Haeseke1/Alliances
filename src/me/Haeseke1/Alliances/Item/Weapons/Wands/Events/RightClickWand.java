59b9c6f509171ab28488257918dfb8956d0fed63
Pieter Herlitska <pieter.herlitska@gmail.com>
2017-02-27 20:28:42 +0100

    Push

--- src/me/Haeseke1/Alliances/Item/Weapons/Wands/Events/RightClickWand.java
+++ src/me/Haeseke1/Alliances/Item/Weapons/Wands/Events/RightClickWand.java
@@ -9,6 +9,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -71,6 +72,4 @@ public class RightClickWand implements Listener{
 			event.setCancelled(true);
 		}
 	}
-
-	
 }
